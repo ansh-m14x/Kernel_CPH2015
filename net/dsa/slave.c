a1acdccbdb8f431d3d622a1b30b1a6a6af094837
ansh-m14x <ansh981786@gmail.com>
2026-05-16 07:29:10 +0530

    118 tag

--- net/dsa/slave.c
+++ net/dsa/slave.c
@@ -1199,6 +1199,9 @@ int dsa_slave_suspend(struct net_device *slave_dev)
 {
 	struct dsa_slave_priv *p = netdev_priv(slave_dev);
 
+	if (!netif_running(slave_dev))
+		return 0;
+	
 	netif_device_detach(slave_dev);
 
 	if (p->phy) {
@@ -1216,6 +1219,9 @@ int dsa_slave_resume(struct net_device *slave_dev)
 {
 	struct dsa_slave_priv *p = netdev_priv(slave_dev);
 
+	if (!netif_running(slave_dev))
+		return 0;
+	
 	netif_device_attach(slave_dev);
 
 	if (p->phy) {
